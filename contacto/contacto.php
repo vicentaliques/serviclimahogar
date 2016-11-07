f843d8d54f82a56b202b64de231dfd3a80fa9f5e
Vicent Aliques <vicentaliques@gmail.com>
2016-11-07 18:03:49 +0100

    introducir página de formulario
    Esta es la página para acabar de cumplimentar el formulario. No esta
    terminada, faltan las verificaciones del correo y las etiquetas de aviso
    de envio o no del correo.

---
+++ contacto/contacto.php
@@ -0,0 +1,19 @@
+<?php
+
+	$para = 'vicentaliques@gmail.com, vicentaliques@hotmail.com';
+	$asunto = 'Mensaje desde web';
+	
+	$mailheader = "From: ".$_POST["email1"]."\r\n";
+	$mailheader .= "Reply-To: ".$_POST["email1"]."\r\n";
+	$mailheader .= "Content-type: text/html;charset=utf-8\r\n";
+
+	$mensaje = "Nombre: " .$_POST["nombre"]."\n";
+	$mensaje .= "\n<br>Email: " .$_POST["email1"]."\n";
+	$mensaje .= "\n<br>Mensaje: " .nl2br($_POST["mensaje"])."\n";
+
+	mail($para, $asunto, $mensaje, $mailheader) or die("Error al enviar el email");
+
+	header('Location: http://www.arttilugios.es/serviclimahogar');
+
+
+?>
