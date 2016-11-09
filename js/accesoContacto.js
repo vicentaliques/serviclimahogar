e25d0664e2a3d1471877bb1e7acbaae4b691f47e
Vicent Aliques <vicentaliques@gmail.com>
2016-11-09 18:10:23 +0100

    Acceso a formulario de contacto desde menu
    Se crea el archivo accesoContacto.js para poder acceder al formulario de
    contacto desde el menu.

---
+++ js/accesoContacto.js
@@ -0,0 +1,13 @@
+ $("#accesoContacto").click(function() {
+    		
+
+						
+
+				document.getElementById("nombreFormCliente").innerHTML = "Gracias por Contactar ";
+				document.getElementById("descripcionEquipo").innerHTML = "Por favor cumplimenta los campos que quedan para que podamos ayudarte";
+				
+				//alert(textoSeleccionado1);
+    			$("#nuevapagina").removeClass("ocultada");
+				
+
+});
